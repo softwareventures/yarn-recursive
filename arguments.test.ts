f7608ef22bb4d59304aec79601b189dc534bb7bc
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-06-18 11:02:12 +0100

    refactor(arguments): add "readArguments" function
    Not used yet.

--- arguments.test.ts
+++ arguments.test.ts
@@ -1,5 +1,5 @@
 import test from "ava";
-import {readArgument} from "./arguments";
+import {readArgument, readArguments} from "./arguments";
 
 test("readArgument", t => {
     t.deepEqual(readArgument("--skip-root"), {type: "option", name: "skipRoot", value: true});
@@ -13,4 +13,24 @@ test("readArgument", t => {
     t.deepEqual(readArgument("--no-cmd"), {type: "option", name: "cmd", value: false});
     t.deepEqual(readArgument("install"), "install");
     t.deepEqual(readArgument("--"), "--");
+});
+
+test("readArguments", t => {
+    t.deepEqual(readArguments([]), {skipRoot: false, includeHidden: false, command: []});
+    t.deepEqual(readArguments(["--skip-root"]), {skipRoot: true, includeHidden: false, command: []});
+    t.deepEqual(readArguments(["--skip-root", "--no-skip-root"]), {skipRoot: false, includeHidden: false, command: []});
+    t.deepEqual(readArguments(["--no-skip-root", "--skipRoot"]), {skipRoot: true, includeHidden: false, command: []});
+    t.deepEqual(readArguments(["install"]), {skipRoot: false, includeHidden: false, command: ["install"]});
+    t.deepEqual(readArguments(["install", "--flat"]),
+        {skipRoot: false, includeHidden: false, command: ["install", "--flat"]});
+    t.deepEqual(readArguments(["--", "install", "--skip-root"]),
+        {skipRoot: false, includeHidden: false, command: ["install", "--skip-root"]});
+    t.deepEqual(readArguments(["--skip-root", "--include-hidden", "--", "install", "--flat"]),
+        {skipRoot: true, includeHidden: true, command: ["install", "--flat"]});
+    t.deepEqual(readArguments(["--include-hidden", "--opt", "--flat", "--cmd", "install"]),
+        {skipRoot: false, includeHidden: true, command: ["install", "--flat"]});
+    t.deepEqual(readArguments(["--opt", "--check-files --flat", "--skip-root", "--cmd", "install"]),
+        {skipRoot: true, includeHidden: false, command: ["install", "--check-files", "--flat"]});
+    t.deepEqual(readArguments(["--cmd", "install --flat"]),
+        {skipRoot: false, includeHidden: false, command: ["install", "--flat"]});
 });
--- arguments.ts
+++ arguments.ts
@@ -1,3 +1,60 @@
+export interface Options {
+    readonly skipRoot: boolean;
+    readonly includeHidden: boolean;
+    readonly command: ReadonlyArray<string>;
+}
+
+export function readArguments(args: ReadonlyArray<string>): Options {
+    let skipRoot = false;
+    let includeHidden = false;
+    let command: string[] = [];
+    let mode: "bare" | "cmd" | "opt" | "passthrough" = "bare";
+
+    for (const arg of args) {
+        if (mode === "passthrough") {
+            command.push(arg);
+        } else if (mode === "cmd") {
+            command = shell(arg).concat(command);
+            mode = "bare";
+        } else if (mode === "opt") {
+            command = command.concat(shell(arg));
+            mode = "bare";
+        } else {
+            const argument = readArgument(arg);
+            if (argument === "--") {
+                mode = "passthrough";
+            } else if (typeof argument === "string") {
+                command.push(argument);
+            } else if (argument.name === "skipRoot") {
+                skipRoot = !!argument.value;
+            } else if (argument.name === "includeHidden") {
+                includeHidden = !!argument.value;
+            } else if (argument.name === "cmd") {
+                if (typeof argument.value === "string") {
+                    command = shell(argument.value).concat(command);
+                } else if (argument.value) {
+                    mode = "cmd";
+                }
+            } else if (argument.name === "opt") {
+                if (typeof argument.value === "string") {
+                    command = command.concat(shell(argument.value));
+                } else if (argument.value) {
+                    mode = "opt";
+                }
+            } else {
+                command.push(arg);
+            }
+        }
+    }
+
+    return {skipRoot, includeHidden, command};
+}
+
+export function shell(arg: string): string[] {
+    // FIXME
+    return arg.split(" ");
+}
+
 export type Argument = Option | string;
 
 export interface Option {
